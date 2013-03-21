9ab8f46a8eec082698a158630d05e98d6c3f0884
fizzlewell <fizzlewell@gmail.com>
2013-03-21 09:35:37 +0800

    Initial Commit

---
+++ Matrix Multi/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Matrix Multi
+//
+//  Created by Liu Wenzhong on 13-3-21.
+//  Copyright (c) 2013年 Liu Wenzhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
